22a00ece3c1af81d754cbcf33a692b9d8b474ac1
clyde.fondop <clyde.fondop@gmail.com>
2018-10-18 13:46:04 -0400

    change user script

--- templates/userdata.sh
+++ templates/userdata.sh
@@ -10,15 +10,15 @@ sudo sde reprovision
 #sudo ./letsencrypt-auto --nginx -d "*.dev.dsolab.net" --agree-tos -m cfondop@securitycompass.com -n
 # https://serverfault.com/questions/750902/how-to-use-lets-encrypt-dns-challenge-validation
 
-sudo /root/.local/bin/aws s3api get-object  --bucket ${bucket_name} --key ${fullchain} /etc/sde/.server.crt
-sudo /root/.local/bin/aws s3api get-object  --bucket ${bucket_name} --key ${private_key} /etc/sde/.server.key
+#sudo /root/.local/bin/aws s3api get-object  --bucket ${bucket_name} --key ${fullchain} /etc/sde/.server.crt
+#sudo /root/.local/bin/aws s3api get-object  --bucket ${bucket_name} --key ${private_key} /etc/sde/.server.key
 
-sudo chmod 664 /etc/sde/.server.crt
-sudo chmod 664 /etc/sde/.server.key
+#sudo chmod 664 /etc/sde/.server.crt
+#sudo chmod 664 /etc/sde/.server.key
 
 # Configure certificate for NGINX
-sudo sed -i 's:/etc/pki/tls/private/sdelements.key:/etc/sde/.server.key:g' /etc/sde/custom.yaml
-sudo sed -i 's:/etc/pki/tls/certs/sdelements.crt:/etc/sde/.server.crt:g' /etc/sde/custom.yaml
+#sudo sed -i 's:/etc/pki/tls/private/sdelements.key:/etc/sde/.server.key:g' /etc/sde/custom.yaml
+#sudo sed -i 's:/etc/pki/tls/certs/sdelements.crt:/etc/sde/.server.crt:g' /etc/sde/custom.yaml
 
 # Configure Password for the user
 pip install awscli --upgrade --user
