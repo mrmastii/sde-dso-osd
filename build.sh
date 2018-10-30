4e7d8a76ecf36310ecfc6130412ae2b4b4c179d5
Clyde Fondop <cfsbarrow@gmail.com>
2018-10-30 13:04:08 -0400

    Update build.sh
    change python3 to python

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # AWS Cli
 BIN_PATH="/home/jenkins/.local/bin" 
 curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
+python get-pip.py --user
 export PATH=$BIN_PATH:$PATH:/sbin
 pip install --user awscli
